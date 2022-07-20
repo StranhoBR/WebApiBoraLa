8a3845495cf832e50efb5bd5fa166bd2638b7a17
StranhoBR <stranhol2@gmail.com>
2022-07-19 23:33:08 -0300

    Criando Profiles

---
+++ WebApiBoraLa/Profiles/SalesOrderProfile.cs
@@ -0,0 +1,17 @@
+﻿using ApplicationServicesSO.Data.Dtos;
+using ApplicationServicesSO.Models;
+using AutoMapper;
+
+namespace ApplicationServicesSO.Profiles
+{
+    public class SalesOrderProfile : Profile
+    {
+        public SalesOrderProfile()
+        {
+            CreateMap<AddEntitiesDto, SalesOrderModel>();
+            CreateMap<SalesOrderModel, RecoverEntitiesDto>();
+            CreateMap<UpdateEntitiesDto, SalesOrderModel>();
+
+        }
+    }
+}
