1dcd401b04b5af719aa2fd2819713482bfca869b
StranhoBR <stranhol2@gmail.com>
2022-07-19 23:30:55 -0300

    Ajustes Iniciais

--- WebApiBoraLa/Program.cs
+++ WebApiBoraLa/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -7,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApiBoraLa
+namespace ApplicationServicesSO
 {
     public class Program
     {
@@ -22,5 +23,6 @@ namespace WebApiBoraLa
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
--- WebApiBoraLa/Startup.cs
+++ WebApiBoraLa/Startup.cs
@@ -1,15 +1,20 @@
+using ApplicationServicesSO.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApiBoraLa
+namespace ApplicationServicesSO
 {
     public class Startup
     {
@@ -23,7 +28,15 @@ namespace WebApiBoraLa
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+
+            //services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("EntitiesfkConnection")));
+            services.AddDbContext<ApplicationContext>(options => options.UseOracle(Configuration.GetConnectionString("EntitiesfkConnection")));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApplicationServicesSO", Version = "v1" });
+            });
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,16 +45,11 @@ namespace WebApiBoraLa
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApplicationServicesSO v1"));
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +57,7 @@ namespace WebApiBoraLa
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
